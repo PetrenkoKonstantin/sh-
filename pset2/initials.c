93628fe89b98404523a164ada653eebed3f70155
Konstantin Petrenko <marker1883@gmail.com>
2016-09-21 20:14:00 +0300

    pset2/task1
    add file initials.c

--- pset2/initials.c
+++ pset2/initials.c
@@ -5,20 +5,20 @@
 
 int main(void){
     const char SPACE = ' ';
-    string name = GetString();                  // функция для ввода строки пользователем
+    string name = GetString();                  // function for string input by the user
    
-   for(int i = 0; i <= strlen(name); i++){      // перевод строки введенной пользователем в верхний регистр
+   for(int i = 0; i <= strlen(name); i++){      // a newline entered by the user to upper case
        if (name[i] > 96){
            name[i] -= 32; 
        }
    }
-           printf("%c", name[0]);               // вывод первой буквы строки 
+           printf("%c", name[0]);               // the output of the first letters of a string 
    
-   for (int y = 0; y < strlen(name); y++){      // определяем заглавные буквы слов в строке
+   for (int y = 0; y < strlen(name); y++){      // determined by the capital letters of words in the string
        if (name[y] == SPACE){
            name[y] = name[y + 1];
            
-           printf("%c", name[y]);               // вывод заглавных букв в строке 
+           printf("%c", name[y]);               // the withdrawal of capital letters in the string 
            
        }
    }
